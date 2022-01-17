1df8a89eacd365bd41aa61faa0ffefc0f03ed65e
Uchchash346 <mru416@gmail.com>
2022-01-17 10:04:50 +0600

    Android animation

--- app/src/main/java/com/example/realtivelayout/MainActivity.java
+++ app/src/main/java/com/example/realtivelayout/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.realtivelayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    ImageView imageView;
+    Animation myAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        imageView = findViewById(R.id.imageView);
+        myAnim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.my_anim);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.startAnimation(myAnim);
+
+            }
+        });
+
     }
 }
