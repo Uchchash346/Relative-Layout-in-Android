133f8c8620db9ab79052f229c0b75d2930c22b05
Uchchash346 <mru416@gmail.com>
2022-01-19 18:03:27 +0600

    library implementation

--- app/src/main/java/com/example/realtivelayout/MainActivity.java
+++ app/src/main/java/com/example/realtivelayout/MainActivity.java
@@ -9,29 +9,23 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
     ImageView imageView;
-    Animation myAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
         imageView = findViewById(R.id.imageView);
-        myAnim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.my_anim);
+        Picasso.get()
+                .load("https://pin.it/5GWuLef")
+                .into(imageView);
 
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                imageView.startAnimation(myAnim);
-
-            }
-        });
 
     }
 }
